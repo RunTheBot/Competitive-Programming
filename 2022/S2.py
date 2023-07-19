430d5575e05911041c2ae650cb1de643785dc18b
RunTheBot <58890327+RunTheBot@users.noreply.github.com>
2023-07-19 18:12:53 -0400

    CCC 2022 S2 Slow

---
+++ 2022/S2.py
@@ -0,0 +1,22 @@
+pairs = []
+enemies = []
+
+violations = 0
+
+for i in range(int(input())):
+    pairs.append(set(input().split(" ")))
+    violations += 1
+
+for i in range(int(input())):
+    enemies.append(set(input().split(" ")))
+
+for i in range(int(input())):
+    group = set(input().split(" "))
+    for i in pairs:
+        if i.issubset(group):
+            violations -= 1
+    for i in enemies:
+        if i.issubset(group):
+            violations += 1
+
+print(violations)
