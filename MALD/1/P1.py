44b8197b497e5b900856c7eb2050c4069a6940f1
RunTheBot <58890327+RunTheBot@users.noreply.github.com>
2023-07-25 19:36:06 -0400

    Im actually dumb

--- MALD/1/P1.py
+++ MALD/1/P1.py
@@ -1,13 +1,18 @@
 from sys import stdin
 import sys
 prev = ""
+donext = False
 out = []
 for i in range(int(stdin.readline())):
-    curr = stdin.readline().lower()
+    curr = stdin.readline()
+    if donext:
+        out.append(curr)
+        donext = False
     if "yubo" in curr:
         out.append(curr)
         if prev != "":
             out.append(prev)
+        donext = True
     prev = curr
 
 for i in sorted(set(out)):
