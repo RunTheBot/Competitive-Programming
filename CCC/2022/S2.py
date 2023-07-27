4f7524c5aa33eac1b17a7ed30107553419ab5713
RunTheBot <58890327+RunTheBot@users.noreply.github.com>
2023-07-26 22:42:35 -0400

    YAY fast

--- CCC/2022/S2.py
+++ CCC/2022/S2.py
@@ -1,22 +1,25 @@
-pairs = []
-enemies = []
-
+from collections import defaultdict
+pairs = defaultdict(set)
+enemies = defaultdict(set)
 violations = 0
 
-for _ in range(int(input())):
-    pairs.append(set(input().split(" ")))
+for i in range(int(input())):
+    pair = input().split()
+    pairs[pair[0]].add(pair[1])
     violations += 1
 
-for _ in range(int(input())):
-    enemies.append(set(input().split(" ")))
+for i in range(int(input())):
+    enemy = input().split()
+    enemies[enemy[0]].add(enemy[1])
 
 for _ in range(int(input())):
-    group = set(input().split(" "))
-    for i in pairs:
-        if i.issubset(group):
-            violations -= 1
-    for i in enemies:
-        if i.issubset(group):
-            violations += 1
+    group = input().split()
+    for person in group:
+        for enemy in enemies[person]:
+            if enemy in group:
+                violations += 1
+        for pair in pairs[person]:
+            if pair in group:
+                violations -= 1
 
 print(violations)
